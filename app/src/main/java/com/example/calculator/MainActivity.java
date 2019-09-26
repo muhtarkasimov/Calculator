724221d876c3825f7d67586338ef2cf13c0f97ee
Muha <muhtarkasimov@gmail.com>
2019-09-27 04:55:25 +0600

    Logically working prototype

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -20,12 +20,16 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     Spinner genderSpinner;
     Spinner activitySpinner;
 
+
+    String[] activities = {"Passive","Medium","Active"};
+    String[] genders= {"Male","Female"};
+
     int age;
     double height;
     double weight;
     double calories;
     double activityCoefficient;
-    boolean male; // true - male, false - female
+    boolean isMale; // true - male, false - female
 
     EditText ageText;
     EditText heightText;
@@ -38,29 +42,31 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //genderSpinner initializing
-        genderSpinner = findViewById(R.id.genderSpinner);
-        ArrayAdapter<CharSequence> genderAdapter = ArrayAdapter.createFromResource(this,
-                R.array.genders, android.R.layout.simple_spinner_item);
-
-        genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        genderSpinner.setAdapter(genderAdapter);
+        //Getting the instance of Spinner and applying OnItemSelectedListener on it
+        genderSpinner = (Spinner) findViewById(R.id.genderSpinner);
         genderSpinner.setOnItemSelectedListener(this);
 
-        //activitySpinner initializing
-        activitySpinner = findViewById(R.id.activitySpinner);
-        ArrayAdapter<CharSequence> activityAdapter = ArrayAdapter.createFromResource(this,
-                R.array.activities, android.R.layout.simple_spinner_item);
+        //Creating the ArrayAdapter instance having the genders list
+        ArrayAdapter genderAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, genders);
+        genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        genderSpinner.setAdapter(genderAdapter);
 
-        activityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        activitySpinner.setAdapter(activityAdapter);
+        //Getting the instance of Spinner and applying OnItemSelectedListener on it
+        activitySpinner = (Spinner) findViewById(R.id.activitySpinner);
         activitySpinner.setOnItemSelectedListener(this);
-        
+
+        //Creating the ArrayAdapter instance having the activities list
+        ArrayAdapter activityAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, activities);
+        activityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        activitySpinner.setAdapter(activityAdapter);
+
         // connecting variables with EditTexts & TextViews
         ageText = (EditText) findViewById(R.id.age);
         heightText = (EditText) findViewById(R.id.heightField);
         weightText = (EditText) findViewById(R.id.weightField);
-        caloriesText = (TextView)findViewById(R.id.caloriesField);
+        caloriesText = (TextView) findViewById(R.id.caloriesField);
 
     }
 
@@ -70,10 +76,24 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         age = Integer.valueOf(ageText.getText().toString());
         height = Double.valueOf(heightText.getText().toString());
         weight = Double.valueOf(weightText.getText().toString());
+        isMale = (genderSpinner.getSelectedItem().toString().equals("Male")) ? true : false;
+
+        if (activitySpinner.getSelectedItem().toString().equals("Passive")) {
+            activityCoefficient = 1.2;
+        } else if (activitySpinner.getSelectedItem().toString().equals("Medium")) {
+            activityCoefficient = 1.55;
+        } else {
+            activityCoefficient = 1.9;
+        }
 
         //BMR = 10W + 6.25H - 5A + 5   (  male  )
         //BMR = 10W + 6.25H - 5A - 161 ( female )
-        calories = (10 * weight) + (6.25 * height) - (5 * age) + 5;
+        if (isMale) {
+            calories = ((10 * weight) + (6.25 * height) - (5 * age) + 5) * activityCoefficient;
+        } else {
+            calories = ((10 * weight) + (6.25 * height) - (5 * age) - 161) * activityCoefficient;
+        }
+
         caloriesText.setText(Double.toString(calories));
     }
 //
@@ -84,24 +104,20 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 ////        caloriesText.setText(a);
 //        Toast.makeText(this, a, Toast.LENGTH_SHORT).show();
 //    }
-//
-//
+////
 //    //works
 //    public void gipgip(View view) {
 //        String a = "gip gip ura";
 //        caloriesText.setText(a);
 //    }
 
-
-
-
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(this, genderSpinner.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this, genderSpinner.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
-        Toast.makeText(this, "Nothing Selected, Baike", Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this, "Nothing Selected, Baike", Toast.LENGTH_SHORT).show();
     }
 }
